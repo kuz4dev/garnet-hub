2755642938431c176672c239a02d9760bf380670
Igor Kazmin <cazmin.igor2015@yandex.ru>
2024-04-17 12:55:59 +0300

    add babel, add sidebar, add intl

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -44,9 +63,10 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     };
 
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
-        fileLoader
+        fileLoader,
     ];
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,8 @@ import { classNames } from 'shared/lib/classNames/classnames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
 
@@ -11,8 +13,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <AppRouter />
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
